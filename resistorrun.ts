8056377ebc4b7db484e6dd8b3dcc187d3fd8b500
Ursula_Visnapuu74 <113088336+ursula-visnapuu@users.noreply.github.com>
2023-03-23 18:15:48 +0200

    Ex 8.4

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,48 +1,58 @@
-class Resistor {
+abstract class AbstractResistor {
+    abstract getResistance(): number;
+    getCurrent(u: number): number{
+        return u/this.getResistance();
+    }
+}
+
+class Resistor extends AbstractResistor {
     r: number = 0;
-    name: String;
-    constructor(r: number, name: String) {
+    constructor(r: number) {
+        super();
         this.r = r;
-        this.name = name;
     }
     getResistance(): number {
         return this.r;
     }
-    getCurrent(u: number): number {
-        return u / this.getResistance();
+}
+
+class Switch extends AbstractResistor {
+    on: boolean = false;
+    setOn(state: boolean){
+        this.on=state;
     }
-    getDescription() {
-        return this.name + " " + this.getResistance() + "ohm";
+    getResistance(): number {
+        return (this.on ? 0 : 1000000000);
+    }
+    getCurrent(u: number): number{
+        if(u>0){
+            if(this.on){
+                throw new Error("Short circuit");
+            }
+        }
+        return super.getCurrent(u);
     }
 }
 
-
-class SeriesCircuit extends Resistor {
-    resistors: Resistor[] = []
-    constructor(name: String) {
-        super(0, name);
-    }
-    push(r: Resistor) {
-        this.resistors.push(r);
-    }
-    getResistance() {
-        let sum: number = 0;
-        this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
-        return sum;
-    }
-
-    getDescription(): string {
-        return this.name + " " + this.getResistance() +  "ohm(" + this.resistors.map(r => r.getDescription()).join(", ") + ")";
-    }
+function printResistance(r: AbstractResistor){
+    console.log(r.getResistance());
 }
 
-let r1: Resistor = new Resistor(10, "takisti1");
-let r2: Resistor = new Resistor(5, "takisti2");
-let r3: Resistor = new Resistor(10, "takisti3");
-let sc1: SeriesCircuit = new SeriesCircuit("jada1");
-let sc2: SeriesCircuit = new SeriesCircuit("jada2");
-sc2.push(r1);
-sc2.push(r2);
-sc1.push(sc2);
-sc1.push(r3);
-console.log(sc1.getDescription());
+function getTotalResistance(elements: AbstractResistor[]){
+    let totalResistance = 0
+    elements.forEach((element) => {
+        totalResistance += element.getResistance()
+    })
+    console.log(totalResistance)
+}
+
+let r1: Switch = new Switch();
+let r2: AbstractResistor = new Resistor(220);
+let elements = []
+elements.push(r1)
+elements.push(r2)
+r1.setOn(true);
+getTotalResistance(elements)
+
+r1.setOn(false);
+getTotalResistance(elements)
