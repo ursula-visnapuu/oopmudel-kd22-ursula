47a20f80f4acdf966a721e84383c3d8b333fce93
Ursula_Visnapuu74 <113088336+ursula-visnapuu@users.noreply.github.com>
2023-03-23 18:13:43 +0200

    Ex 8.2 ja 8.3

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,7 +1,9 @@
 class Resistor {
     r: number = 0;
-    constructor(r: number) {
+    name: String;
+    constructor(r: number, name: String) {
         this.r = r;
+        this.name = name;
     }
     getResistance(): number {
         return this.r;
@@ -9,13 +11,16 @@ class Resistor {
     getCurrent(u: number): number {
         return u / this.getResistance();
     }
+    getDescription() {
+        return "Resistor " + this.name;
+    }
 }
 
 
 class SeriesCircuit extends Resistor {
     resistors: Resistor[] = []
-    constructor() {
-        super(0);
+    constructor(name: String) {
+        super(0, name);
     }
     push(r: Resistor) {
         this.resistors.push(r);
@@ -25,21 +30,26 @@ class SeriesCircuit extends Resistor {
         this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
         return sum;
     }
+
+    getDescription(): string {
+        return "SeriesCircuit " + this.name + "(" + this.resistors.map(r => r.getDescription()).join(", ") + ")";
+    }
 }
 
-let r1: Resistor = new Resistor(100);
-let r2: Resistor = new Resistor(150);
-let r3: Resistor = new Resistor(175);
-let sc1: SeriesCircuit = new SeriesCircuit();
-let sc2: SeriesCircuit = new SeriesCircuit();
+let r1: Resistor = new Resistor(100, "t1");
+let r2: Resistor = new Resistor(150, "t2");
+let r3: Resistor = new Resistor(175, "t3");
+let r4: Resistor = new Resistor(175, "t4");
+let sc1: SeriesCircuit = new SeriesCircuit("j1");
+let sc2: SeriesCircuit = new SeriesCircuit("j2");
+let sc3: SeriesCircuit = new SeriesCircuit("j3");
 sc1.push(r1);
 sc2.push(r2);
 sc2.push(r3);
+sc3.push(r4);
+sc2.push(sc3)
 sc1.push(sc2);
-console.log(sc1.getResistance());
-console.log(sc1.getCurrent(12));
-console.log('---------------------------')
-let r4: Resistor = new Resistor(10);
-sc2.push(r4)
-console.log(sc2.getResistance());
-console.log(sc1.getResistance());
+console.log(sc1.getDescription());
+console.log(sc3.getResistance())
+console.log(sc2.getResistance())
+console.log(sc1.getResistance())
